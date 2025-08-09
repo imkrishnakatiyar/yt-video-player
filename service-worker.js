bbaef2089c0ebf690244e63e558191af57a514b0
imkrishnakatiyar <90976299+imkrishnakatiyar@users.noreply.github.com>
2025-08-09 18:38:09 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open('orpheus-video-cache-v23').then((cache) => {
+        caches.open('orpheus-video-cache-v24').then((cache) => {
             return cache.addAll([
                 '/',
                 '/index.html',
@@ -25,7 +25,7 @@ self.addEventListener('activate', (event) => {
         caches.keys().then((cacheNames) => {
             return Promise.all(
                 cacheNames.map((cache) => {
-                    if (cache !== 'orpheus-video-cache-v23') {
+                    if (cache !== 'orpheus-video-cache-v24') {
                         return caches.delete(cache);
                     }
                 })
